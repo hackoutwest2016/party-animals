1f0e8cbeb0cf3b702863f97d9688e8119e5d33b3
Pierre Reimertz <pierre@pierrereimertz.com>
2016-08-10 11:55:40 +0200

    add hax

--- both/routes.js
+++ both/routes.js
@@ -27,7 +27,7 @@ Router.route('/watch/:id', function () {
 
 Router.route('/game/:_id', function () {
   let game = Games.findOne({_id: this.params._id});
-  console.log("In router", game);
+  if(!game) return;
   this.layout('defaultLayout');
   this.render('playGame', {data: {game: game}});
 });
